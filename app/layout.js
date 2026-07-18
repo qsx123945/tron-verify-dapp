61f2fb778eeb2eff36dfa5fe4dc63e3dbac029d9
qsx123945 <kanglongceng@gmail.com>
2026-07-18 23:45:15 +0800

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "TRON Verify DApp",
+  description: "Telegram TRON Wallet Verification",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="zh">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
